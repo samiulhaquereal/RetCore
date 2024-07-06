4350316723e4cf931f30b6f0193abbf241da4c37
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-07-07 00:27:35 +0600

    version: 0.0.1

--- lib/src/features/retcore_routing.dart
+++ lib/src/features/retcore_routing.dart
@@ -1,10 +1,21 @@
 import 'package:retcore/src/config/imports.dart';
 
-class RetCore{
+class RetCorePageRoute{
+
+  static RetCorePageRoute? _instance;
+  RetCorePageRoute._internal();
+  factory RetCorePageRoute(){
+    _instance ??= RetCorePageRoute._internal();
+    return _instance!;
+  }
 
   static void to(Widget page){
     BuildContext? context = FindContext.getContext();
     Navigator.push(context!,MaterialPageRoute(builder: (context) => page));
-}
+  }
+  static void back(dynamic result){
+    BuildContext? context = FindContext.getContext();
+    Navigator.of(context!).pop(result);
+  }
 
 }
