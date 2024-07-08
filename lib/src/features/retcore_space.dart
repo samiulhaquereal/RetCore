fed9a08477a4bced9d3c91a2bb5baf201d08d9f7
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-07-08 21:08:40 +0600

    version: 0.0.1

--- lib/src/features/retcore_space.dart
+++ lib/src/features/retcore_space.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:retcore/src/config/imports.dart';
 
 class RetcoreSpace{
@@ -10,9 +9,40 @@ class RetcoreSpace{
     return _instance!;
   }
 
-  static Widget retcoreSpace(double? size) {
+ /* static Widget retcoreSpace(double? size) {
     BuildContext? context = FindContext.getContext();
     final direction = _getParentDirection(context!);
+
+    if (direction == Axis.vertical) {
+      return SizedBox(height: size ?? 0);
+    } else if (direction == Axis.horizontal) {
+      return SizedBox(width: size ?? 0);
+    } else {
+      // Default to vertical if the direction couldn't be determined
+      return SizedBox(height: size ?? 0);
+    }
+  }
+
+  static Axis _getParentDirection(BuildContext context) {
+    final parent = context.findAncestorWidgetOfExactType<Flex>();
+    if (parent != null) {
+      return parent.direction;
+    }
+    final wrap = context.findAncestorWidgetOfExactType<Wrap>();
+    if (wrap != null) {
+      return wrap.direction;
+    }
+    // Default to vertical if no parent direction is found
+    return Axis.vertical;
+  }*/
+
+  static Widget retcoreSpace(double? size) {
+    BuildContext? context = RetCoreNavigatorKey.currentContext;
+    if (context == null) {
+      return SizedBox(height: size ?? 0);
+    }
+
+    final direction = _getParentDirection(context);
 
     if (direction == Axis.vertical) {
       return SizedBox(height: size ?? 0);
