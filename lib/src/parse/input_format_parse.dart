fce81ab043a79e446e744f9cc9b4cc435cbc8f56
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2024-07-24 23:30:34 +0600

    version: 0.0.2

--- lib/src/parse/input_format_parse.dart
+++ lib/src/parse/input_format_parse.dart
@@ -1,28 +1,33 @@
 import 'package:retcore/src/config/imports.dart';
 
-class RetCoreInputTypeParse{
-
+class RetCoreInputTypeParse {
   static RetCoreInputTypeParse? _instance;
+
   RetCoreInputTypeParse._internal();
-  factory RetCoreInputTypeParse(){
+
+  factory RetCoreInputTypeParse() {
     _instance ??= RetCoreInputTypeParse._internal();
     return _instance!;
   }
 
   static TextInputFormatter InputType({RetCoreInputType? type, int? wordLimit}) {
-    final Map<String, TextInputFormatter> formatters = {
-      'text': FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]')),
-      'double': FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]')),
-      'int': FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-      'email': FilteringTextInputFormatter.allow(
+    final formatters = {
+      RetCoreInputType.text:
+          FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]')),
+      RetCoreInputType.double:
+          FilteringTextInputFormatter.allow(RegExp(r'[0-9\.]')),
+      RetCoreInputType.int: FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+      RetCoreInputType.email: FilteringTextInputFormatter.allow(
         RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'),
       ),
-      'password': FilteringTextInputFormatter.allow(
-        RegExp(r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).+$'),
+      RetCoreInputType.password: FilteringTextInputFormatter.allow(
+        RegExp(
+            r'^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%^&*(),.?":{}|<>]).+$'),
       ),
-      'word': WordInputFormatter(maxWords: wordLimit ?? 10)
+      RetCoreInputType.word: WordInputFormatter(maxWords: wordLimit ?? 10),
     };
-    return formatters[type?.toString().split('.').last] ?? FilteringTextInputFormatter.deny(RegExp(r'.'));
+
+    return formatters[type] ?? FilteringTextInputFormatter.deny(RegExp(r'.'));
   }
 }
 
@@ -33,14 +38,16 @@ class WordInputFormatter extends TextInputFormatter {
 
   @override
   TextEditingValue formatEditUpdate(
-      TextEditingValue oldValue,
-      TextEditingValue newValue,
-      ) {
-    final newWords = newValue.text.split(RegExp(r'\s+')).where((word) => word.isNotEmpty).toList();
+    TextEditingValue oldValue,
+    TextEditingValue newValue,
+  ) {
+    final newWords = newValue.text
+        .split(RegExp(r'\s+'))
+        .where((word) => word.isNotEmpty)
+        .toList();
     if (newWords.length > maxWords) {
       return oldValue;
     }
     return newValue;
   }
 }
-
